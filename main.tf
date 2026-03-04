b128255e062bab6ba2340f4a64d8bb9e0076ded8
Manu Jayakumar <99114381+manujayakumar@users.noreply.github.com>
2026-03-05 02:11:29 +0530

    Added web-file

--- main.tf
+++ main.tf
@@ -2,3 +2,68 @@ resource "aws_s3_bucket" "website" {
   bucket = var.bucket_name
 }
 
+# Make S3 bucket private
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "website-oac"
+  description                       = "website policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudFront" {
+  bucket = aws_s3_bucket.website.id
+
+  depends_on = [aws_s3_bucket_public_access_block.block]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Upload website files to S3
+resource "aws_s3_object" "website_files" {
+  for_each = fileset("${path.module}/website", "**/*")
+
+  bucket = aws_s3_bucket.website.id
+  key    = each.value
+  source = "${path.module}/www/${each.value}"
+  etag   = filemd5("${path.module}/www/${each.value}")
+  content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "json" = "application/json",
+    "png"  = "image/png",
+    "jpg"  = "image/jpeg",
+    "jpeg" = "image/jpeg",
+    "gif"  = "image/gif",
+    "svg"  = "image/svg+xml",
+    "ico"  = "image/x-icon",
+    "txt"  = "text/plain"
+  }, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
+}
